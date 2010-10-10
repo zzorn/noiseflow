a56504c8a4638435f88586c20d31f3d445bb0369
zzorn <zzorn@iki.fi>
2010-10-10 23:14:41 +0300

    Implemented editing and binding of numerical parameters in the UI.

--- src/main/scala/org/noiseflow/ui/graph/NodeView.scala
+++ src/main/scala/org/noiseflow/ui/graph/NodeView.scala
@@ -3,24 +3,29 @@ package org.noiseflow.ui.graph
 import org.noiseflow.node.Node
 import org.scalaprops.Property
 import net.miginfocom.swing.MigLayout
-import javax.swing.{BorderFactory, JLabel, JPanel}
-import java.awt.Color
-import java.awt.event.{MouseAdapter, MouseEvent, MouseMotionAdapter}
+import javax.swing._
+import event.{ChangeEvent, ChangeListener}
+import java.awt.event._
+import java.awt.{Dimension, Graphics, Color}
 
 /**
  * 
  */
-class NodeView(node: Node, tableView: TableView) extends JPanel {
+// TODO: Extract property view
+class NodeView(val node: Node, tableView: TableView) extends JPanel {
 
   var xPos = 0
   var yPos = 0
 
-  setLayout(new MigLayout("wrap 2"))
+  private var propertySources: Map[Property[_], PropertySource] = Map()
+  private var propertyTargets: Map[Property[_], PropertyTarget] = Map()
+
+  setLayout(new MigLayout("wrap 3, insets null null null 0"))
 
   setBorder(BorderFactory.createLineBorder(Color.BLACK))
   add(new JLabel(node.name()), "wrap")
 
-  node.properties foreach (e => addPropertyView(e._1, e._2))
+  node.properties.filterNot(_._1 == 'name) foreach (e => addPropertyView(e._1, e._2))
 
   var dragStartScreen: (Int, Int) = (0,0)
   var dragStartComponent: (Int, Int) = (0,0)
@@ -41,10 +46,128 @@ class NodeView(node: Node, tableView: TableView) extends JPanel {
 
   def addPropertyView(name: Symbol, property: Property[_]) {
 
-    add(new JLabel(name.name))
-    add(new JLabel(property.value.toString))
+    // Name label
+    val propertySource = new PropertySource(property)
+    propertySources += property -> propertySource
+    add(propertySource)
 
-    //add(new PropertyView(name, property))
+    // Value view / editor
+    // TODO: Add type to properties - probably name also, and a ref to the parent bean - a bit heavier, but much more useful
+    property.value match {
+      case v: Double => add(createNumberSpinner(property))
+      case _         => add(createReadOnlyLabel(property))
+    }
+
+    // Binding widget
+    val propertyTarget = new PropertyTarget(property, tableView)
+    propertyTargets += property -> propertyTarget
+    add(propertyTarget)
+  }
+
+  def hasProperty(p: Property[_]): Boolean = propertyTargets.contains(p)
+
+  def getPropertyInPos(p: Property[_]): (Int, Int) = propertyTargets(p).inPoint
+
+  def getPropertyOutPos(p: Property[_]): (Int, Int) = propertySources(p).outputPos
+
+  class PropertySource(val property: Property[_]) extends JLabel(property.name.name) {
+    def outputPos = (getLocationOnScreen.x - tableView.getLocationOnScreen.x,
+                     getLocationOnScreen.y - tableView.getLocationOnScreen.y + getHeight / 2 )
+  }
+
+  def getPropertyAt(x: Int, y: Int): Option[Property[_]] = {
+    getComponentAt(x, y) match {
+      case null => None
+      case ps: PropertySource => if (ps.property.value.isInstanceOf[Double]) Some(ps.property) else None
+      case _ => None
+    }
+  }
+
+  class PropertyTarget(val property: Property[_], tableView: TableView) extends JPanel()  {
+    setPreferredSize(new Dimension(16, 16))
+
+    def inPoint = (bindPointX, bindPointY)
+
+    def bindPointX = getLocationOnScreen.x - tableView.getLocationOnScreen.x + getWidth
+    def bindPointY = getLocationOnScreen.y - tableView.getLocationOnScreen.y + getHeight / 2
+
+    var bindingDragActive = false
+
+    if (property.value.isInstanceOf[Double]) {
+      addMouseMotionListener(new MouseMotionAdapter(){
+        override def mouseDragged(e: MouseEvent) = {
+          tableView.setBindingPreviewLine(bindPointX, bindPointY,
+            e.getXOnScreen - tableView.getLocationOnScreen.x,
+            e.getYOnScreen - tableView.getLocationOnScreen.y)
+        }
+      })
+      addMouseListener(new MouseAdapter(){
+        override def mousePressed(e: MouseEvent) = {
+          bindingDragActive = true
+        }
+
+        override def mouseReleased(e: MouseEvent) = {
+          if (bindingDragActive) {
+            bindingDragActive = false
+            tableView.getPropertyAt(e.getXOnScreen - tableView.getLocationOnScreen.x,
+                                    e.getYOnScreen - tableView.getLocationOnScreen.y) match {
+              case None => property.unbind
+              case Some(other: Property[_]) =>
+                // TODO: Check type compability
+                property.asInstanceOf[Property[Double]].bind(other.asInstanceOf[Property[Double]], automaticUpdate = false)
+            }
+
+            tableView.hideBindingPreviewLine()
+          }
+        }
+      })
+    }
+
+    override def paintComponent(g: Graphics) = {
+      g.setColor(Color.BLACK)
+      g.fillOval(0,0,getWidth(), getHeight())
+    }
+  }
+
+
+  private def createNumberSpinner(property: Property[_]): JSpinner = {
+    val initialValue: Double = property.value.asInstanceOf[Double]
+    val model = new SpinnerNumberModel(initialValue, null, null, initialValue.abs.floor * 0.1 + 0.1)
+    model.addChangeListener(new ChangeListener() {
+      def stateChanged(e: ChangeEvent) = {
+        val v = model.getValue.asInstanceOf[Double]
+        model.setStepSize(v.abs.floor * 0.1 + 0.1)
+        property.asInstanceOf[Property[Double]].set(v)
+      }
+    })
+
+    // NOTE: Updates too fast, eats performance - what do?
+    //property.onChange((p: Property[_]) => model.setValue(p.value.asInstanceOf[Double]))
+
+    val spinner = new JSpinner(model)
+    spinner.setPreferredSize(new Dimension(80, 24))
+    spinner.addMouseWheelListener(new MouseWheelListener() {
+      def mouseWheelMoved(e: MouseWheelEvent) = {
+        val scroll = -e.getWheelRotation
+        var i = 0
+        while (i < scroll) {
+          model.setValue(model.getNextValue)
+          i += 1
+        }
+        while (i > scroll) {
+          model.setValue(model.getPreviousValue)
+          i -= 1
+        }
+      }
+    })
+
+    spinner
+  }
+
+  def createReadOnlyLabel(property: Property[_]): JLabel = {
+    val label = new JLabel(property.value.toString)
+    property.onChange((p: Property[_]) => label.setText(p.value.toString))
+    label
   }
 
 
--- src/main/scala/org/noiseflow/ui/graph/TableView.scala
+++ src/main/scala/org/noiseflow/ui/graph/TableView.scala
@@ -1,10 +1,10 @@
 package org.noiseflow.ui.graph
 
 import javax.swing.JPanel
-import org.scalaprops.Bean
 import org.noiseflow.node.{Node, Table}
-import java.awt.Graphics
 import net.miginfocom.swing.MigLayout
+import org.scalaprops.{Property, Bean}
+import java.awt.{Color, Component, Graphics}
 
 /**
  * View of a composite node consisting of other nodes.
@@ -15,6 +15,9 @@ class TableView(table: Table) extends JPanel {
 
   private var insertX = 10
   private var insertY = 10
+
+  private var bindingPreviewLineVisible = false
+  private var bindingPreviewLine = (0,0,0,0)
 
   val migLayout: MigLayout = new MigLayout()
   setLayout(migLayout)
@@ -60,8 +63,56 @@ class TableView(table: Table) extends JPanel {
     repaint()
   }
 
+  def getPropertyAt(x: Int, y: Int): Option[Property[_]] = {
+    getComponentAt(x, y) match {
+      case null => None
+      case n: NodeView => n.getPropertyAt(x - n.getX, y - n.getY)
+      case _ => None
+    }
+  }
+
+  def setBindingPreviewLine(x1: Int, y1: Int, x2: Int, y2: Int) {
+    bindingPreviewLine = (x1, y1, x2, y2)
+    bindingPreviewLineVisible = true
+    repaint()
+  }
+
+  def hideBindingPreviewLine() {
+    bindingPreviewLineVisible = false
+    repaint()
+  }
+
   override def paintComponent(g: Graphics) = {
     // TODO: Draw connection lines
     g.clearRect(0,0, getWidth, getHeight)
+    
+    views.values foreach {nv =>
+      nv.node.properties.values foreach { p =>
+        val bound = p.boundProperty
+        if (bound != null) {
+          val from = nv.getPropertyInPos(p)
+          views.values.find( nv2 => nv2.hasProperty(bound)) match {
+            case None =>
+            case Some(nv3: NodeView) =>
+              val to = nv3.getPropertyOutPos(bound)
+              g.setColor(Color.BLACK)
+              g.drawLine(from._1, from._2,
+                         to._1,   to._2)
+          }
+        }
+      }
+    }
+  }
+
+
+  override def paint(g: Graphics) = {
+    super.paint(g)
+
+    if (bindingPreviewLineVisible) {
+      g.setColor(Color.BLACK)
+      g.drawLine(bindingPreviewLine._1, bindingPreviewLine._2,
+                 bindingPreviewLine._3, bindingPreviewLine._4)
+    }
+
   }
 }
